3278b3360806dc9b89538a5f1d02eb0b014e8428
rfuke <rfuke@loffacorp.com>
2021-06-08 23:45:22 -0700

    add Weather API to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
+// create city var for search form
+var getCitiRepos = function (city) {
+ var apiURL = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q=" + city + "&appid=82f4ffefa4407cc347782857301fcc38"
+
+// make request for the URL
+fetch(apiURL).then(function(response){
+    response.json().then(function(data){
+        console.log(data);
+    });
+});
+};
+getCitiRepos("Scottsdale");
+
+var weatherFormEl = document.querySelector ("#form-weather");
+var citiInputEl = document.querySelector("#findCity");
+
 var formSubmitHandler = function(event){
     event.preventDefault();
     console.log(event)
 }
+
+weatherFormEl.addEventListener("submit", formSubmitHandler);
